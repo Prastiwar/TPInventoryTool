f373f12c2396424a82d827b9dfbdb42d05a5fc0b
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-03-05 13:14:04 +0100

    Numerate + copyrights

--- TPInventoryTool/Assets/TP_Creator/TP_InventoryCreator/Scripts/TPInventoryCreator.cs
+++ TPInventoryTool/Assets/TP_Creator/TP_InventoryCreator/Scripts/TPInventoryCreator.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections.Generic;
+﻿/**
+*   Authored by Tomasz Piowczyk
+*   Copyright 2018 You're allowed to make changes in functionality and use for commercial or personal.
+*   You're not allowed to claim ownership of this script. 
+*   https://github.com/Prastiwar/TPInventoryTool
+*   Unity 2017.3.0f
+*/
+using System.Collections.Generic;
 using TP.InventoryEditor;
 using TP.Utilities;
 using UnityEngine;
